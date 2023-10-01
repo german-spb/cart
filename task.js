f0e98a21a526770b51b7d6b50140259fbbc8d2cc
German <german@pushkin-spb.com>
2023-10-01 23:04:26 +0300

    commit second

--- task.js
+++ task.js
@@ -4,6 +4,9 @@ const quantity = document.getElementsByClassName('product__quantity-value')
 const add = document.getElementsByClassName('product__add')
 const products = document.getElementsByClassName('product')
 const images = document.getElementsByClassName('product__image')
+const cart = document.querySelector('.cart__products')
+// const cartCount = document.getElementsByClassName('cart__product-count')
+
 for (let i = 0; i< plus.length; i++){
     plus[i].onclick = () => {
     quantity[i].textContent = Number(quantity[i].textContent)+1
@@ -14,7 +17,26 @@ for (let i = 0; i< plus.length; i++){
         }
     }
     add[i].onclick = () => {
+        // console.log(products[i].getAttribute('data-id'))
+        // console.log(images[i].getAttribute('src'))
+        const cartCount = document.querySelector('.cart__product-count')
+        const productInCart = document.querySelectorAll('.cart__product')
         console.log(products[i].getAttribute('data-id'))
-        console.log(images[i].getAttribute('src'))
+        console.log()
+        if (cartCount != null && products[i].getAttribute('data-id') != 0 ){
+            cartCount.textContent = Number(cartCount.textContent) + Number(quantity[i].textContent)
+            cart.innerHTML += `
+            <div class="cart__product-count">${cartCount.textContent}</div>`
+          
+            
+        }
+        else {
+        cart.innerHTML +=  
+       `<div class="cart__product" data-id="${products[i].getAttribute('data-id')}">
+           <img class="cart__product-image" src="${images[i].getAttribute('src')}">
+           <div class="cart__product-count">${quantity[i].textContent}</div>
+       </div>`}
+         
+        // console.log(cartCount)
     }
 }
